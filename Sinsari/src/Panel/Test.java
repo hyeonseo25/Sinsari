edc19130073ac53c3523806e59d0285f9f539c81
hyeon <s2019s30@e-mirim.hs.kr>
2020-11-14 22:30:20 +0900

    first commit

---
+++ Sinsari/src/Panel/Test.java
@@ -0,0 +1,15 @@
+package Panel;
+
+import javax.swing.JFrame;
+
+public class Test extends JFrame{
+	public Test() {
+		setVisible(true);
+		setSize(500,500);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Test();
+	}
+
+}
